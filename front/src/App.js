494ad0bf6497cef07b38b96085cd5f79f82fa15c
rypetrat <rypetrat@bu.edu>
2024-02-28 17:35:31 -0500

    actually didnt add the src and public files lmao oopsies force push

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-text">
+
+        <>
+          Hey team welcome to the frontend of our Health Diagnostic tool!
+        </>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
